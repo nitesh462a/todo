56b623482c8b37352aac224c76c9fd8edbe4bd71
nitesh462a <niteshkumaryts@gmail.com>
2025-06-27 07:17:32 +0530

    wait

--- src/app/reset-password/page.jsx
+++ src/app/reset-password/page.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import { Suspense } from 'react';
 
 import ResetPasswordClient from './ResetPasswordClient';
 export default function ResetPasswordPage() {
