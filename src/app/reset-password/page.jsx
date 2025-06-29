ee06b00cdcb18d0c7d45dfd027232af3261e08d7
nitesh462a <niteshkumaryts@gmail.com>
2025-06-29 14:01:22 +0530

    reset

---
+++ src/app/reset-password/page.jsx
@@ -0,0 +1,10 @@
+import ResetPasswordClient from "../resetpasswordclient/page"
+
+const page = () => {
+  return (
+    <div >hello</div>
+    // <ResetPasswordClient />
+  )
+}
+
+export default page
