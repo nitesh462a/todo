c1f99611d7a9e04f4ce8e916196585490dd7c66a
nitesh462a <niteshkumaryts@gmail.com>
2025-06-29 14:27:49 +0530

    reset

--- src/app/reset-password/page.jsx
+++ src/app/reset-password/page.jsx
@@ -1,54 +1,13 @@
-'use client';
-import { useState } from 'react';
-import axios from 'axios';
-import { useSearchParams } from 'next/navigation';
-import Link from 'next/link';
+import ResetPasswordClient from "../resetclient/page";
 
-export default function ResetPasswordClient() {
-  
-  const token = useSearchParams().get('token');
 
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
+import { Suspense } from 'react';
 
-  const handleReset = async () => {
-    try {
-      const res = await axios.post('/api/reset-password', {
-        token,
-        password
-      });
-      setMessage(res.data.message);
-    } catch (err) {
-      setMessage('❌ Error resetting password.');
-    }
-  };
 
+export default function ResetPasswordPage() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
-      <div className="bg-white shadow-xl rounded-2xl p-8 max-w-md w-full">
-        <h2 className="text-2xl font-bold mb-4 text-center">🔑 Reset Password</h2>
-        <input
-          type="password"
-          placeholder="Enter new password"
-          className="w-full px-4 py-2 border rounded-lg mb-4"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button
-          onClick={handleReset}
-          className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition"
-        >
-          Reset Password
-        </button>
-
-        <Link href={"/login"}>go to login</Link>
-
-        {message && (
-          <div className="mt-4 text-center text-sm text-green-600 font-medium">
-            {message}
-          </div>
-        )}
-      </div>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <ResetPasswordClient />
+    </Suspense>
   );
 }
